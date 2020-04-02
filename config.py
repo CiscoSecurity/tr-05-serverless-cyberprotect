26995bc7a685a5ea9de7e397c367c915383b6b35
GeorgiyII <gosha1104@gmail.com>
2020-04-02 12:41:40 +0300

    added health check logic

--- config.py
+++ config.py
@@ -7,3 +7,13 @@ class Config:
     VERSION = VERSION
 
     SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+    CYBERPROTECT_API_URL = \
+        'https://threatscore.cyberprotect.fr/api/score/{observable}'
+
+    CYBERPROTECT_HEALTH_CHECK_IP = '127.0.0.100'
+
+    CYBERPROTECT_OBSERVABLE_TYPES = {
+        'ip': 'IP',
+        'ipv6': 'IPV6',
+    }
