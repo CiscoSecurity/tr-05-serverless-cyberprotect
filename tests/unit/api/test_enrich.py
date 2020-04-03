a9b5bbb43a8060faf8f3202fbc23ed1d55d0b2d1
GeorgiyII <gosha1104@gmail.com>
2020-04-03 14:05:06 +0300

    added tests

--- tests/unit/api/test_enrich.py
+++ tests/unit/api/test_enrich.py
@@ -1,6 +1,17 @@
 from http import HTTPStatus
 
 from pytest import fixture
+from unittest import mock
+
+from tests.unit.mock_for_tests import (
+    CYBERPROTECT_RESPONSE,
+    CYBERPROTECT_500_ERROR_RESPONSE_MOCK,
+    CYBERPROTECT_404_ERROR_RESPONSE_MOCK,
+    EXPECTED_RESPONSE_500_ERROR,
+    EXPECTED_RESPONSE_404_ERROR,
+    EXPECTED_RESPONSE_DELIBERATE,
+    EXPECTED_RESPONSE_OBSERVE
+)
 
 
 def routes():
@@ -11,6 +22,33 @@ def routes():
 @fixture(scope='module', params=routes(), ids=lambda route: f'POST {route}')
 def route(request):
     return request.param
+
+
+@fixture(scope='function')
+def cyberprotect_api_request():
+    with mock.patch('requests.get') as mock_request:
+        yield mock_request
+
+
+def cyberprotect_api_response(*, ok, status_error=None):
+    mock_response = mock.MagicMock()
+
+    mock_response.ok = ok
+
+    if ok:
+        payload = CYBERPROTECT_RESPONSE
+
+    else:
+        if status_error == 404:
+            payload = CYBERPROTECT_404_ERROR_RESPONSE_MOCK
+            mock_response.status_code = 404
+        elif status_error == 500:
+            payload = CYBERPROTECT_500_ERROR_RESPONSE_MOCK
+            mock_response.status_code = 500
+
+    mock_response.json = lambda: payload
+
+    return mock_response
 
 
 @fixture(scope='module')
@@ -25,9 +63,46 @@ def test_enrich_call_with_invalid_json_failure(route, client, invalid_json):
 
 @fixture(scope='module')
 def valid_json():
-    return [{'type': 'domain', 'value': 'cisco.com'}]
+    return [{'type': 'ip', 'value': '1.1.1.1'}]
 
 
-def test_enrich_call_success(route, client, valid_json):
+@fixture(scope='module')
+def expected_payload(route, client):
+
+    payload = None
+
+    if route.startswith('/deliberate'):
+
+        payload = EXPECTED_RESPONSE_DELIBERATE
+
+    if route.startswith('/observe'):
+
+        payload = EXPECTED_RESPONSE_OBSERVE
+
+    return payload
+
+
+def test_enrich_call_success(route, client, valid_json,
+                             cyberprotect_api_request, expected_payload):
+    cyberprotect_api_request.return_value = cyberprotect_api_response(ok=True)
+
+    response = client.post(route, json=valid_json)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == expected_payload
+
+
+def test_enrich_call_404(route, client, valid_json, cyberprotect_api_request):
+    cyberprotect_api_request.return_value = \
+        cyberprotect_api_response(ok=False, status_error=404)
     response = client.post(route, json=valid_json)
     assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_404_ERROR
+
+
+def test_enrich_call_500(route, client, valid_json, cyberprotect_api_request):
+    cyberprotect_api_request.return_value = \
+        cyberprotect_api_response(ok=False, status_error=500)
+    response = client.post(route, json=valid_json)
+    assert response.status_code == 200
+    assert response.get_json() == EXPECTED_RESPONSE_500_ERROR
