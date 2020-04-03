49bae9f500b82192f784a4341f52ee3f921c0bcc
GeorgiyII <gosha1104@gmail.com>
2020-04-03 14:04:52 +0300

    added verdicts mapping

--- api/enrich.py
+++ api/enrich.py
@@ -1,26 +1,147 @@
-from functools import partial
+from datetime import datetime, timedelta
+from uuid import uuid4
 
-from flask import Blueprint
+import requests
+from flask import Blueprint, current_app
 
 from api.schemas import ObservableSchema
-from api.utils import get_json, jsonify_data
+from api.utils import get_json, jsonify_data, url_for, get_response_data
 
 enrich_api = Blueprint('enrich', __name__)
 
 
-get_observables = partial(get_json, schema=ObservableSchema(many=True))
+def validate_cyberprotect_output(cyberprotect_input):
+    url = url_for(cyberprotect_input)
+    return get_response_data(requests.get(url))
+
+
+def group_observables(relay_input):
+    # Leave only unique pairs.
+
+    result = []
+    for obj in relay_input:
+
+        obj['type'] = obj['type'].lower()
+
+        # Get only supported types.
+        if obj['type'] in current_app.config['CYBERPROTECT_OBSERVABLE_TYPES']:
+            if obj in result:
+                continue
+            result.append(obj)
+
+    return result
+
+
+def get_cyberprotect_outputs(observables):
+    # Return list of responses from AbuseIPDB for all observables
+
+    outputs = []
+    for observable in observables:
+        cyberprotect_output = validate_cyberprotect_output(
+            observable['value'])
+
+        cyberprotect_output['observable'] = observable
+        outputs.append(cyberprotect_output)
+
+    return outputs
+
+
+def get_disposition(score):
+
+    disposition = None
+    disposition_name = None
+
+    for d_name, borders in \
+            current_app.config['CYBERPROTECT_SCORE_RELATIONS'].items():
+        if borders[0] <= score <= borders[1]:
+            disposition = current_app.config['CTIM_DISPOSITIONS'][d_name]
+            disposition_name = d_name
+
+    return disposition, disposition_name
+
+
+def extract_verdicts(output, score):
+    disposition, disposition_name = get_disposition(score['score'])
+
+    start_time = datetime.strptime(score['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    end_time = start_time + timedelta(
+        days=current_app.config['CTIM_VALID_DAYS_PERIOD'])
+
+    valid_time = {
+        'start_time': start_time.isoformat() + 'Z',
+        'end_time': end_time.isoformat() + 'Z'
+    }
+
+    observable = {
+        'value': output['observable']['value'],
+        'type': output['observable']['type']
+    }
+
+    doc = {
+        'observable': observable,
+        'disposition': disposition,
+        'disposition_name': disposition_name,
+        'valid_time': valid_time,
+        **current_app.config['CTIM_VERDICT_DEFAULTS']
+    }
+
+    return doc
+
+
+def format_docs(docs):
+    return {'count': len(docs), 'docs': docs}
 
 
 @enrich_api.route('/deliberate/observables', methods=['POST'])
 def deliberate_observables():
-    _ = get_observables()
-    return jsonify_data({})
+    relay_input = get_json(ObservableSchema(many=True))
+
+    observables = group_observables(relay_input)
+
+    if not observables:
+        return jsonify_data({})
+
+    cyberprotect_outputs = get_cyberprotect_outputs(observables)
+
+    verdicts = []
+    for output in cyberprotect_outputs:
+        for score in output['scores']:
+            # need to check because [[]] return in output if don't have scores
+            if score:
+                verdicts.append(extract_verdicts(output, score))
+
+    relay_output = {}
+
+    if verdicts:
+        relay_output['verdicts'] = format_docs(verdicts)
+
+    return jsonify_data(relay_output)
 
 
 @enrich_api.route('/observe/observables', methods=['POST'])
 def observe_observables():
-    _ = get_observables()
-    return jsonify_data({})
+    relay_input = get_json(ObservableSchema(many=True))
+
+    observables = group_observables(relay_input)
+
+    if not observables:
+        return jsonify_data({})
+
+    cyberprotect_outputs = get_cyberprotect_outputs(observables)
+
+    verdicts = []
+    for output in cyberprotect_outputs:
+        for score in output['scores']:
+            # need to check because [[]] return in output  if don't have scores
+            if score:
+                verdicts.append(extract_verdicts(output, score))
+
+    relay_output = {}
+
+    if verdicts:
+        relay_output['verdicts'] = format_docs(verdicts)
+
+    return jsonify_data(relay_output)
 
 
 @enrich_api.route('/refer/observables', methods=['POST'])
--- config.py
+++ config.py
@@ -7,9 +7,32 @@ class Config:
     CYBERPROTECT_API_URL = \
         'https://threatscore.cyberprotect.fr/api/score/{observable}'
 
+    CYBERPROTECT_SOURCE_NAME = 'Threatscore Cyberprotect'
+
     CYBERPROTECT_HEALTH_CHECK_IP = '127.0.0.100'
 
     CYBERPROTECT_OBSERVABLE_TYPES = {
         'ip': 'IP',
         'ipv6': 'IPV6',
+    }
+
+    CYBERPROTECT_SCORE_RELATIONS = {
+        'Unknown': (0, 0),
+        'Clean': (0.001, 0.25),
+        'Suspicious': (0.251, 0.5),
+        'Malicious': (0.501, 1)
+    }
+
+    CTIM_SCHEMA_VERSION = '1.0.16'
+    CTIM_VERDICT_DEFAULTS = {
+        'type': 'verdict',
+    }
+
+    CTIM_VALID_DAYS_PERIOD = 7
+
+    CTIM_DISPOSITIONS = {
+        'Clean': 1,
+        'Suspicious': 3,
+        'Malicious': 2,
+        'Unknown': 5
     }
