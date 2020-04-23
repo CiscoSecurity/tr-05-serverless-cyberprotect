37cc5d2845a595a8acc803f03b882d83f256e5f9
GeorgiyII <gosha1104@gmail.com>
2020-04-23 10:43:26 +0300

    changed CTIM_ENTITIES_MAX_LIMIT to CTR_ENTITIES_LIMIT

--- config.py
+++ config.py
@@ -46,13 +46,13 @@ class Config:
         'severity': 'Medium',
     }
 
-    CTIM_DEFAULT_ENTITIES_LIMIT = 100
-    CTIM_MAX_ENTITIES_LIMIT = CTIM_DEFAULT_ENTITIES_LIMIT
+    CTR_DEFAULT_ENTITIES_LIMIT = 100
+    CTR_ENTITIES_LIMIT = CTR_DEFAULT_ENTITIES_LIMIT
 
     try:
         limit = int(os.environ.get('CTR_ENTITIES_LIMIT'))
         if limit > 0:
-            CTIM_MAX_ENTITIES_LIMIT = limit
+            CTR_ENTITIES_LIMIT = limit
     except (ValueError, TypeError):
         pass
 
