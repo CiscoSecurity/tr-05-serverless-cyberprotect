e48a2e522a8dbdcabc34b34846724a122032d5cc
YuriiTsekhovyi <ytsek@softserveinc.com>
2021-06-03 09:50:08 +0300

    Add 404 error handling and warning to CTR_ENTITIES_LIMIT variable

--- code/api/utils.py
+++ code/api/utils.py
@@ -1,5 +1,6 @@
 from functools import wraps
 
+from json.decoder import JSONDecodeError
 import jwt
 import json
 import requests
@@ -55,6 +56,7 @@ def get_public_key(jwks_host, token):
     expected_errors = {
         ConnectionError: WRONG_JWKS_HOST,
         InvalidURL: WRONG_JWKS_HOST,
+        JSONDecodeError: WRONG_JWKS_HOST,
     }
     try:
         response = requests.get(f"https://{jwks_host}/.well-known/jwks")
